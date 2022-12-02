1dc6b39dc6c898f06af12f6a27fd0e589e69a95d
SEM <SEM@DESKTOP-4GKM6HK>
2022-12-02 11:35:26 +0300

    Proje dosyası ekle.

---
+++ Scope2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = 100;
+Başla();
+
+void Başla()
+{
+    
+    Console.WriteLine("x ilk değeri" + x);
+    IkiyeKatla(x);
+    Console.WriteLine("x metoddan döndükten sonrası değeri" + x);
+
+}
+
+void IkiyeKatla(int a)
+{
+    a = a * 2;
+    Console.WriteLine("x metod içi değeri" + a);
+}
